fc84cea162043b580cb4cea11c94f59778532831
--replace-all <--replace-all>
2022-10-12 23:40:36 +0530

    inti

--- src/controllers/v1/participant/participants.ts
+++ src/controllers/v1/participant/participants.ts
@@ -4,6 +4,8 @@ import { SuccessResponse } from "../../../core/ApiResponse";
 import ParticipantRepo from "../../../database/repository/ParticipantRepo";
 import asyncHandler from "../../../utils/asyncHandler";
 
+import { ParticipantRole} from "../../../database/model/Participant";
+
 export const getCandidates = asyncHandler(async (req: Request, res: Response) => {
 	const getInterviews = req.query.interviews === "true" || false;
 	const pRepo = getCustomRepository(ParticipantRepo);
@@ -16,4 +18,24 @@ export const getInterviewers = asyncHandler(async (req: Request, res: Response) 
 	const pRepo = getCustomRepository(ParticipantRepo);
 	const interviewers = await pRepo.getInterviwers(getInterviews);
 	new SuccessResponse("success", interviewers).send(res);
+});
+
+export const addCandidates = asyncHandler(async (req: Request, res: Response) => {
+	const pRepo = getCustomRepository(ParticipantRepo);
+	const getInterviews = req.query.interviews == "true" || false;
+	
+	await pRepo.save(req.body);
+	// res.send({status:"ok",message:"api is running"});
+	const candidates = await pRepo.getCandidates(getInterviews);
+	new SuccessResponse("success", candidates).send(res);
+});
+
+export const addInterviewers = asyncHandler(async (req: Request, res: Response) => {
+	const pRepo = getCustomRepository(ParticipantRepo);
+	const getInterviews = req.query.interviews == "true" || false;
+	console.log(req.body);
+	await pRepo.save(req.body);
+	const interviewers = await pRepo.getInterviwers(getInterviews);
+	new SuccessResponse("success", interviewers).send(res);
+	// res.send({status:"ok",message:"api is running"});
 });
--- src/routes/v1/participant/participants.ts
+++ src/routes/v1/participant/participants.ts
@@ -1,9 +1,23 @@
 import express from "express";
-import { getCandidates, getInterviewers } from "../../../controllers/v1/participant/participants";
+import { getCandidates, getInterviewers, addCandidates, addInterviewers } from "../../../controllers/v1/participant/participants";
 
 const router = express.Router();
 
 router.get("/candidates", getCandidates);
 router.get("/interviewers", getInterviewers);
+
+
+
+
+// 
+
+router.post("/candidate",addCandidates);
+router.post("/interviewer",addInterviewers);
+
+
+
+
+
+// 
 
 export default router;
